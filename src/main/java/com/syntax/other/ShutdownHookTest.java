e0d75797321b7b1ef2744d2a6460c0b7167d6e49
ayzhouwen <644988358@qq.com>
2019-02-13 15:43:55 +0800

    signal

--- src/main/java/com/syntax/other/ShutdownHookTest.java
+++ src/main/java/com/syntax/other/ShutdownHookTest.java
@@ -1,10 +1,14 @@
 package com.syntax.other;
 
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
 import java.util.concurrent.TimeUnit;
 
 //测试优雅关机
 public class ShutdownHookTest {
-    public static void main(String[] args) throws InterruptedException {
+    //ShutdownHook方式
+    public static void test1(){
         Runtime.getRuntime().addShutdownHook(new Thread(()->{
 
             System.out.println("优雅关机:ShutdownHook开始");
@@ -17,8 +21,37 @@ public class ShutdownHookTest {
 
         },"优雅关机"));
 
-        TimeUnit.SECONDS.sleep(7);
+        try {
+            TimeUnit.SECONDS.sleep(7);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        System.exit(0);
+    }
+
+    //signal方式
+    public static void test2(){
+        Signal sig=new Signal(System.getProperties().getProperty("os.name").toLowerCase().startsWith("win")?"INT":"TERM");
+        Signal.handle(sig, new SignalHandler() {
+            @Override
+            public void handle(Signal signal) {
+                System.out.println("优雅关机:Signal开始");
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("优雅关机:Signal结束");
+            }
+        });
+
+        try {
+            TimeUnit.SECONDS.sleep(7);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        ShutdownHookTest.test2();
     }
 }
