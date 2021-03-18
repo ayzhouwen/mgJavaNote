6b80eada22e726484519c44739e21d541dc17ea7
zhouwen <644988358@qq.com>
2021-03-18 11:12:50 +0800

    字符串作为锁key 对其进行并发测试

---
+++ src/main/java/com/meConcurrent/lock/StringLockTest.java
@@ -0,0 +1,75 @@
+package com.meConcurrent.lock;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 字符串作为锁测试
+ *
+ */
+
+@Slf4j
+public class StringLockTest {
+
+    private ExecutorService pool = Executors.newFixedThreadPool(16);
+    //累计和
+    private static Integer sum =0;
+    //并发计算次数
+    private static  int sumCount=50000;
+    private static CountDownLatch countDownLatch=new CountDownLatch(sumCount);
+
+    public void  test1(){
+
+
+        for (int i = 0; i <sumCount ; i++) {
+            //不调用set方法,默认走常量池,用 name作为锁key,线程安全
+            Req req=new Req("abc".intern());
+            //new String不走常量池,用name字段 作为锁key,线程不安全
+            req.setName(new String("abc"));
+            pool.execute(new MyRunable(req));
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        log.info("sum 最终合计值:"+sum);
+        pool.shutdownNow();
+
+
+    }
+    @Data
+    class  Req {
+        private String name;
+        public Req(String name){
+            this.name=name;
+        }
+    }
+
+    class MyRunable implements Runnable{
+        private Req req;
+        public MyRunable(Req req){
+            this.req=req;
+        }
+
+        @Override
+        public void run() {
+            //字符串作为锁key时,一定要加上intern,即使name字段是new 出来的,调用此方法后,会从常量池里取,线程安全
+            synchronized (req.name.intern()){
+                sum++;
+                log.info(sum +"");
+                countDownLatch.countDown();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        StringLockTest test=new  StringLockTest();
+        test.test1();
+    }
+}
