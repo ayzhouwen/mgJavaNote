5e69c13d312a94cefc1d24da92c8de2354584295
zhouwen <644988358@qq.com>
2021-11-12 10:59:04 +0800

    有序HashSet演示

---
+++ src/main/java/com/syntax/Collections/LinkHashSetTest.java
@@ -0,0 +1,43 @@
+package com.syntax.Collections;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * LinkHashSet 测试
+ */
+@Slf4j
+public class LinkHashSetTest {
+    /**
+     * HashSet插入无序演示
+     */
+    public static void test1(){
+        Set<String> ipSet=new HashSet<>();
+        ipSet.add(null);
+        ipSet.add("1.168.53.22");
+        ipSet.add("192.168.22.23");
+        ipSet.add("5.168.53.24");
+        ipSet.add("192.168.77.25");
+        log.info("无序ipSet:"+ JSON.toJSONString(ipSet));
+    }
+
+
+    public static void test2(){
+        Set<String> ipSet=new LinkedHashSet<>();
+        ipSet.add(null);
+        ipSet.add("1.168.53.22");
+        ipSet.add("192.168.22.23");
+        ipSet.add("5.168.53.24");
+        ipSet.add("192.168.77.25");
+        log.info("有序ipSet:"+ JSON.toJSONString(ipSet));
+    }
+
+    public static void main(String[] args) {
+        LinkHashSetTest.test1();
+        LinkHashSetTest.test2();
+    }
+}
