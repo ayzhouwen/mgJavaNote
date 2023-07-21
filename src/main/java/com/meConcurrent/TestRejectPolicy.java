91d09b21d4f467a1070e3a9ec426e90110b3e6b9
zw <644988358@qq.com>
2023-07-21 16:32:02 +0800

    线程池拒绝策略增加阻塞场景测试

--- src/main/java/com/meConcurrent/TestRejectPolicy.java
+++ src/main/java/com/meConcurrent/TestRejectPolicy.java
@@ -4,9 +4,7 @@ package com.meConcurrent;
 
 //参考:https://www.jianshu.com/p/aa420c7df275
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 //ThreadPoolExecutor.AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。
 // ThreadPoolExecutor.DiscardPolicy：也是丢弃任务，但是不抛出异常。
@@ -102,11 +100,66 @@ public class TestRejectPolicy {
         }
     }
 
+    /**
+     * 测试由于决绝策略带来的线程阻塞问题，
+     * 参考：https://mp.weixin.qq.com/s/C4SKZ_K5QsApdH9LooPffA
+     * 这个代码在生产环境中需要几处修改
+     * 1.不能使用CallerRunsPolicy ,因为极端情况可能会更糟糕，比如业务代码bug导致来一个线程阻塞一个
+     * 2.无论什么阻塞get，都要加上超时，这是最后兜底的操作
+     * 3.这种情况 线程池在 try finally{ pool.shutdownNow()} 比较好，能较好的关闭线程池
+     */
+    void test5(){
+        // 一个核心线程，队列最大为1，最大线程数也是1.拒绝策略是DiscardPolicy
+        ThreadPoolExecutor executorService = new ThreadPoolExecutor(1, 1, 1L, TimeUnit.MINUTES,
+                new ArrayBlockingQueue<>(1), new ThreadPoolExecutor.AbortPolicy());
+
+        Future f1 = executorService.submit(()-> {
+            System.out.println("提交任务1");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Future f2 = executorService.submit(()->{
+            System.out.println("提交任务2");
+        });
+
+        Future f3 = executorService.submit(()->{
+            System.out.println("提交任务3");
+        });
+
+        try {
+            System.out.println("任务1完成 " + f1.get());// 等待任务1执行完毕
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            System.out.println("任务2完成" + f2.get());// 等待任务2执行完毕
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            System.out.println("任务3完成" + f3.get());// 等待任务3执行完毕
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        executorService.shutdown();// 关闭线程池，阻塞直到所有任务执行完毕
+    }
+
 
 
 
     public static void main(String[] args) {
         TestRejectPolicy testRejectPolicy=new TestRejectPolicy();
-        testRejectPolicy.test4();
+        testRejectPolicy.test5();
     }
 }
