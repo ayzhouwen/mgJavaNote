aa529851bc84e23e4e1152e7a29ab2d43eed4aca
zw <644988358@qq.com>
2023-11-03 16:30:46 +0800

    log写入测试和文件写入测试，单线程logback同步写性能能达到100万3秒，自己写文件用bufferedWriter单线程也可以做到

--- src/main/java/com/io/file/TestFileDesciptor.java
+++ src/main/java/com/io/file/TestFileDesciptor.java
@@ -1,23 +1,50 @@
 package com.io.file;
 
 
-
 import cn.hutool.core.date.TimeInterval;
+import com.util.MyDateUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 //测试文件描述符,在Windows平台是fd是-1,handle是wind的handle
+
+/**
+ *  Files.write 测试
+ * 固态加锁写入测试
+ * 2023-11-03 14:32:22 [Thread-0]-> 线程:Thread-0已追加写入10000行，字符串测试长度:10000,耗时:6466.0毫秒,6.47秒,0.11分钟
+ * 2023-11-03 14:32:23 [Thread-1]-> 线程:Thread-1已追加写入10000行，字符串测试长度:10000,耗时:6742.0毫秒,6.74秒,0.11分钟
+ * <p>
+ * 固态不加锁测试
+ * 2023-11-03 14:31:15 [Thread-1]-> 线程:Thread-1已追加写入10000行，字符串测试长度:10000,耗时:4950.0毫秒,4.95秒,0.08分钟
+ * 2023-11-03 14:31:15 [Thread-0]-> 线程:Thread-0已追加写入10000行，字符串测试长度:10000,耗时:5020.0毫秒,5.02秒,0.08分钟
+ *
+ *
+ * bufferedWriter 测试
+ * 固态不加锁写入测试，如过加锁性能反而会更快，因为是多次批量写入，（能达到0.8秒，但是实际观测起来不及时，都是成批写入）
+ * 2023-11-03 15:54:34 [pool-1-thread-2]-> 线程:pool-1-thread-2已追加写入10000行，字符串测试长度:10000,耗时:1014.0毫秒,1.01秒,0.02分钟
+ * 2023-11-03 15:54:34 [pool-1-thread-1]-> 线程:pool-1-thread-1已追加写入10000行，字符串测试长度:10000,耗时:1014.0毫秒,1.01秒,0.02分钟
+ */
+@Slf4j
 public class TestFileDesciptor {
     //测试在win和linux平台fd 和handle的不同
-    public  static void   test(){
-     //   File file=new File(TestFileDesciptor.class.getResource("/test.txt").getPath()); //有斜杠则是根路径
+    public static void test() {
+        //   File file=new File(TestFileDesciptor.class.getResource("/test.txt").getPath()); //有斜杠则是根路径
         try {
             System.out.println(TestFileDesciptor.class.getResource("/test.txt").getPath());
-            FileInputStream fileInputStream=new FileInputStream(TestFileDesciptor.class.getResource("/test.txt").getPath());
-            FileDescriptor fileDescriptor=fileInputStream.getFD();
+            FileInputStream fileInputStream = new FileInputStream(TestFileDesciptor.class.getResource("/test.txt").getPath());
+            FileDescriptor fileDescriptor = fileInputStream.getFD();
             Field[] fields = FileDescriptor.class.getDeclaredFields();
-            StringBuffer stringBuffer=new StringBuffer();
+            StringBuffer stringBuffer = new StringBuffer();
             //获取私有属性
             for (int i = 0; i < fields.length; i++) {
                 fields[i].setAccessible(true);
@@ -31,19 +58,19 @@ public class TestFileDesciptor {
 
             System.out.println(stringBuffer.toString());
             System.out.println("开始打开文件");
-            int num=1;
+            int num = 1;
             String path;
             TimeInterval timer = new TimeInterval(true);
 
-            for (int i=0;i<num;i++){
-                fileInputStream=new FileInputStream(TestFileDesciptor.class.getResource("/test2.txt").getPath());
+            for (int i = 0; i < num; i++) {
+                fileInputStream = new FileInputStream(TestFileDesciptor.class.getResource("/test2.txt").getPath());
                 // fileInputStream=new FileInputStream("D:\\zw\\mgJavaNote\\src\\main\\resources\\test.txt");
-              //  path=TestFileDesciptor.class.getResource("/test.txt").getPath();
+                //  path=TestFileDesciptor.class.getResource("/test.txt").getPath();
 
             }
 
             //System.out.println(num+"个文件全部打开完毕");
-            System.out.println(num+"次"+timer.interval());//花费毫秒数
+            System.out.println(num + "次" + timer.interval());//花费毫秒数
             //通过FileDescriptor获取流
 
         } catch (FileNotFoundException e) {
@@ -54,20 +81,125 @@ public class TestFileDesciptor {
 
     }
 
-    //测试通过文件描述符进行读写,读取test1的内容进行加工后写入test3,然后在全部读取出来
-    public  static  void test2(){
-        FileInputStream fileInputStream;
-        try {
-             fileInputStream=new FileInputStream(TestFileDesciptor.class.getResource("/test1.txt").getPath());
-            BufferedInputStream in = null;
-            BufferedOutputStream out = null;
+    //多线程测试追加文件，看会不会造成写冲突
+    public static void fileWriteTest() {
+        String fileName = "example.txt";
+        String[] wFlag = {"a", "b"};
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        Path filePath = Paths.get(fileName);
+        try {
+            Files.delete(filePath);
+        } catch (IOException e) {
+
         }
+        for (int i = 0; i < wFlag.length; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                try {
+                    //字符串长度
+                    int wLen = 100;
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < wLen; j++) {
+                        sb.append(wFlag[finalI]);
+                    }
+                    sb.append(System.lineSeparator());
+                    //写入次数
+                    int wCount = 10000;
+                    long start = System.currentTimeMillis();
+                    for (int j = 0; j < wCount; j++) {
+
+                        //windows上不加锁10000字符串长度会出现串行即ab活ba的情况
+//                        synchronized (TestFileDesciptor.class){
+//                            Files.write(filePath, sb.toString().getBytes(),StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+//                        }
+                        Files.write(filePath, ("j行:"+j+sb.toString()).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+
+                    }
+
+                    log.info("线程:{}已追加写入{}行，字符串测试长度:{},耗时{}", Thread.currentThread().getName(), wCount, wLen, MyDateUtil.execTime("", start));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }).start();
+
+        }
+
     }
+
+    //
+
+    /**
+     * BufferedWriter 在单线程立即刷新下能达到100万，3秒速度，基本跟logback同步写日志性能持平
+     * 注意；多线程下BufferedWriter必须加锁，并且每个线程必须操作的是同一个BufferedWriter对象
+     */
+    public static void BufferedWriterTest() {
+        String fileName = "example.txt";
+        String[] wFlag = {"a", "b"};
+
+        Path filePath = Paths.get(fileName);
+        try {
+            Files.delete(filePath);
+        } catch (IOException e) {
+
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath.toString()))) {
+            // 创建线程池以并发写入文件
+            ExecutorService executorService = Executors.newFixedThreadPool(wFlag.length);
+            CountDownLatch count=new CountDownLatch(wFlag.length);
+            for (int i = 0; i < wFlag.length; i++) {
+                int finalI = i;
+                executorService.execute(() -> {
+                    //字符串长度
+                    int wLen = 10000;
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < wLen; j++) {
+                        sb.append(wFlag[finalI]);
+                    }
+                    sb.append(System.lineSeparator());
+                    //写入次数
+                    int wCount = 10000;
+                    long start = System.currentTimeMillis();
+
+                    for (int j = 0; j < wCount; j++) {
+//                        synchronized (TestFileDesciptor.class){
+                            try {
+                                bufferedWriter.append( "j行:"+j+sb.toString());
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            try {
+                                bufferedWriter.flush();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+//                        }
+
+                    }
+                    log.info("线程:{}已追加写入{}行，字符串测试长度:{},耗时{}", Thread.currentThread().getName(), wCount, wLen, MyDateUtil.execTime("", start));
+                    count.countDown();
+                });
+
+            }
+
+            count.await();
+            executorService.shutdown();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 
     public static void main(String[] args) {
-        test();
+//        test();
+//        fileWriteTest();
+        BufferedWriterTest();
     }
 }
+
+
--- src/main/java/com/syntax/SyntaxTest.java
+++ src/main/java/com/syntax/SyntaxTest.java
@@ -1,111 +1,98 @@
 package com.syntax;
 
-import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.io.checksum.CRC16;
-import cn.hutool.core.util.ByteUtil;
-import cn.hutool.core.util.HexUtil;
-import com.util.CRCUtils;
-import lombok.Data;
-import lombok.Getter;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.util.MyDateUtil;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import java.io.IOException;
-import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * @author admin
  */
 //临时随手代码测试类,可以忽略次类d
-
-
-class MyDog<A extends String, B, C, D, E, F> {
-    A getDog() {
-        return (A) "AAAAA";
-    }
-}
-
-@FunctionalInterface
-interface SFunction<T, R> extends Function<T, R>, Serializable {
-}
-
 @Slf4j
 public class SyntaxTest {
 
     int i;
+    static  String str="";
+//    public static void main(String[] args)  {
+//        for (int j = 0; j < 2; j++) {
+//            new Thread(()->{
+//                Long i=1L;
+//                while (true){
+//                    i++;
+//                    str+=i;
+////                    log.info(str);
+//                    if (i%100000==0){
+//                        str="";
+//                    }
+//                }
+//            }).start();
+//        }
+//
+//        int port = RandomUtil.randomInt(8000,9000); // 选择要绑定的端口号
+//        ServerSocket serverSocket = null;
+//        try {
+//             serverSocket = new ServerSocket(port);
+//            System.out.println("当前服务监听端口 " + port);
+//
+//            while (true) {
+//                Socket clientSocket = serverSocket.accept(); // 阻塞等待客户端连接
+//                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+//
+//                // 在这里可以处理客户端请求，例如读取/写入数据
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }finally {
+//            try {
+//                serverSocket.close();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//    }
 
-    public void incr() {
-        synchronized (this) {
-            i++;
-        }
-    }
+    public static void main(String[] args) {
+//        new Thread(()->{
+//            try {
+//                for (int j = 0; j <3 ; j++) {
+//                    Thread.sleep(1000*30);
+//                    long start=System.currentTimeMillis();
+//                    int size=1000000;
+//                    for (int i = 0; i <size ; i++) {
+//                        log.info("完成写入:"+i);
+//                    }
+//                    log.info(MyDateUtil.execTime("执行"+size+"次",start));
+//                }
+//
+//
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//        }).start();
 
-    public static void main(String[] args)  {
-
-        for (int j = 0; j < 2; j++) {
-            new Thread(()->{
-                Long i=1L;
-                while (true){
-                    i++;
-                }
-            }).start();
-        }
-
-        int port = 9080; // 选择要绑定的端口号
-        ServerSocket serverSocket = null;
         try {
-             serverSocket = new ServerSocket(port);
-            System.out.println("当前服务监听端口 " + port);
-
-            while (true) {
-                Socket clientSocket = serverSocket.accept(); // 阻塞等待客户端连接
-                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
-
-                // 在这里可以处理客户端请求，例如读取/写入数据
+            for (int j = 0; j <3 ; j++) {
+                Thread.sleep(1000*30);
+                long start=System.currentTimeMillis();
+                int size=1000000;
+                for (int i = 0; i <size ; i++) {
+                    log.info("完成写入:"+i);
+                }
+                log.info(MyDateUtil.execTime("执行"+size+"次",start));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            try {
-                serverSocket.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
     }
-
-
-    /**
-     * 获取线程快照信息
-     *
-     * @return
-     */
-    public static String getJavaStackTrace() {
-        StringBuffer msg = new StringBuffer();
-        for (Map.Entry<Thread, StackTraceElement[]> stackTrace : Thread.getAllStackTraces().entrySet()) {
-            Thread thread = (Thread) stackTrace.getKey();
-            StackTraceElement[] stack = (StackTraceElement[]) stackTrace.getValue();
-            if (thread.equals(Thread.currentThread())) {
-                continue;
-            }
-            msg.append("\n 线程:").append(thread.getName()).append("\n");
-            for (StackTraceElement element : stack) {
-                msg.append("\t").append(element).append("\n");
-            }
-        }
-        return msg.toString();
-    }
-
 
 }
 
